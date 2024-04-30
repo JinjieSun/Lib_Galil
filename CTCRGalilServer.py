1fbd4ada56e5b54c8a1329097ebfc1baa058b1f8
Jinjie Sun MEDCVR UTM 07 <jinjie.sun@mail.utoronto.ca>
2024-04-30 17:21:33 -0400

    add scripts for Homing and move the robot to hardcode start pos

--- CTCRGalilServer.py
+++ CTCRGalilServer.py
@@ -6,6 +6,28 @@ import numpy as np
 import time
 
 
+DELTAQ = np.zeros((6,))
+# START_POS = [0, 10, 0, 6, 0, 3]
+START_POS = [0, 40, 0, 32, 0, 20]
+
+
+def GoHome(rob, update_Q):
+    print("Homing the Robot")
+    if np.any(update_Q != np.zeros((6,))):
+        rob.jointPTPLinearMotionSinglePoint(-update_Q, sKurve=True, sKurveValue=0.004)
+    print("Homing Finished")
+    return update_Q - update_Q
+
+def goStartPos(rob, update_Q):
+    if np.any(update_Q != np.zeros((6,))):
+        update_Q = GoHome(rob, update_Q)
+    print("Moving to Start Position")
+    rob.jointPTPLinearMotionSinglePoint(START_POS, sKurve=True, sKurveValue=0.004)
+    update_Q += START_POS
+    print("'-------------- Current Joint Info --------------")
+    print(rob.getJointPositions())
+    return update_Q
+
 
 def InitRobot():
     if ROB_EXIST:
@@ -20,9 +42,11 @@ def InitRobot():
 
 
 def Connect(rob):
-    host, port = "100.112.72.236", 8190
+    
+    port = 8190
     bytes_to_recieve = 53411
-
+    update_Q = np.zeros((6,))
+    
     # SOCK_STREAM means TCP socket
     # Connect to the server and send the data
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,20 +56,21 @@ def Connect(rob):
     conn, addr = sock.accept()
     with conn:
         print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            # print(data.decode())
-            delta_q = DecodeMessage(data)
-            result = SendMovementCommand(rob, delta_q)
-            # print(decoded_msg)
-            conn.send(bytes((result,)))
-            
-    print("Quit!")               
-    Exit(rob)
-
-
+        goStartPos(rob, update_Q)
+        try:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                # print(data.decode())
+                delta_q = DecodeMessage(data)
+                result, update_Q = SendMovementCommand(rob, delta_q, update_Q)
+                # print(decoded_msg)
+                conn.send(bytes((result,)))
+            print("Quit!")     
+        except:
+            print("Error Occur During connection")
+        Exit(rob, update_Q)
 
 
 def DecodeMessage(data):
@@ -55,33 +80,38 @@ def DecodeMessage(data):
     msg_lst = data.decode().split(" ")
     if (len(msg_lst) != 7):
         print("Incorrect msg received")
-    # Update Version     
+    # Update Version
     elif msg_lst[0] == "spos":
-        delta_q = [int(float(num)) for num in msg_lst[1:]]
+        delta_q = [float(num) for num in msg_lst[1:]]
         print(delta_q)
+    
     return MapToGalil(delta_q)
 
 
-def SendMovementCommand(rob, delta_q):
+def SendMovementCommand(rob, delta_q, update_Q):
     if np.sum(delta_q) == 0:
+        print(delta_q)
         print("No motion")
-        return False
+        return False, update_Q
     rob.setMotorConstraints('MaxSpeed', 150000)
     rob.setMotorConstraints('MaxAcc', 1000000)
     rob.setMotorConstraints('MaxDec', 1000000)
     print('-------------- Motion: {} --------------'.format(delta_q))
     #tracker._BEEP(1)
+    
+    update_Q += delta_q
     rob.jointPTPLinearMotionSinglePoint(delta_q, sKurve=True, sKurveValue=0.004)
     
     print("'-------------- Current Joint Info --------------")
     print(rob.getJointPositions())
     # time.sleep(.5)
-    return True
+    return True, update_Q
 
 
-def Exit(rob):
+def Exit(rob, update_Q):
     print('-------------- close robot and measurement system --------------')
     # robot
+    _ = GoHome(rob, update_Q)  
     if ROB_EXIST:
         rob.motorsOff()
         rob.disconnect()
@@ -117,6 +147,10 @@ def MapToGalil(delta_q):
 if __name__ == '__main__':
     
     # Initialization
+
+  
+
+
     ROB_EXIST = True
     TRACKER_EXIST = False
     n_measurements = 10
